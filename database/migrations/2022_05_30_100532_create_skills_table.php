7fdb6414aa590965f9b2f8269f6b13b2dd5986dd
tariq-shreem <tariqshreem00@gmail.com>
2022-05-30 13:22:21 +0300

    create migrations

---
+++ database/migrations/2022_05_30_100532_create_skills_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('skills', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',50);
+            $table->string('img',50);
+            $table->boolean('active')->default(true);
+            $table->foreignId('cat_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('skills');
+    }
+};
---
+++ database/migrations/2022_05_30_100641_create_exams_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exams', function (Blueprint $table) {
+            $table->id();
+            $table->text('name');
+            $table->text('desc');
+            $table->string('img');
+            $table->tinyInteger('questions_no');
+            $table->tinyInteger('difficulty');
+            $table->smallInteger('duration_mins');
+            $table->boolean('active')->default(true);
+            $table->foreignId('skill_id')->constrained();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exams');
+    }
+};
